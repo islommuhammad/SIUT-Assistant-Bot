086596bc5af9a795b831237b5883a408180b8e0d
Islom Mamatov <salomshox@gmail.com>
2022-11-01 04:54:52 +0500

    Beta version

--- start.py
+++ start.py
@@ -22,10 +22,10 @@ for i in data['staffs']:
         person = (i["name"])
 try:
     print(person)
-    bot.send_photo(-321996347, photo=open('rasm.jpg', 'rb'),caption="🎉🎉🎉*Happy birthday!!!* \n\n🎂Today is birthday of *{}!* \n Congratulations! \n\n*Our social networks:* \n[WWW](https://www.siut.uz/index.php) ▫️ [Instagram](https://www.instagram.com/siut.uz/) ▫️ [Facebook](https://www.facebook.com/siut.uz) ▫️ [Telegram](https://t.me/siut_uz)\n©️ Copyright: @Islom\_Mamatov".format(person), parse_mode='Markdown')
+    bot.send_photo(-321996347, photo=open('rasm.jpg', 'rb'),caption="🎉🎉🎉*Happy birthday!!!* \n\n🎂Today is birthday of *{}!* \n Congratulations! 👏👏👏👏👏+\n\n*Our social networks:* \n[WWW](https://www.siut.uz/index.php) ▫️ [Instagram](https://www.instagram.com/siut.uz/) ▫️ [Facebook](https://www.facebook.com/siut.uz) ▫️ [Telegram](https://t.me/siut_uz)\n©️ Copyright: @Islom\_Mamatov".format(person), parse_mode='Markdown')
 
 except:
-    print ("Today nobody's birthday")
+    print ("Today is nobody's birthday")
 
 f.close()
 
@@ -36,8 +36,6 @@ f.close()
 #def send_message(message):
 # bot.send_photo(-1001712122376, photo=open('robot.jpg', 'rb'),caption=
 #     "🎉🎉🎉*Hello world!!!* \n\n I am ***SIUT Assistant Bot.*** I'm created by SIUT IT Center!!! \n\n*I can do:* \n✅ Remind about the staff birthdays; 🎂 \n✅ Remind about important days and holidays, etc \n\n _I'm under construction. Hasta la vista, baby. I'll be back 👍_  \n\n*Our social networks:* \n[WWW](https://www.siut.uz/index.php) ▫️ [Instagram](https://www.instagram.com/siut.uz/) ▫️ [Facebook](https://www.facebook.com/siut.uz) ▫️ [Telegram](https://t.me/siut_uz)\n©️ Copyright: @Islom\_Mamatov", parse_mode='Markdown')
-
-
 
 
 print("Success!!!")
