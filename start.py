83b08c24b5110b0dcb08674c9f6bc797cfc8abbd
Islom <salomshox@gmail.com>
2022-11-14 04:18:05 +0500

    aaa

--- start.py
+++ start.py
@@ -16,7 +16,9 @@ def main():
     kun = today.strftime("%d-%m-%Y")
     # path = os.path.abspath(os.getcwd())
     path = os.path.dirname(__file__)
+    
     f = open(path+'/staffs.json')
+    print(f)
     data=json.load(f)
 
     for i in data['staffs']:
