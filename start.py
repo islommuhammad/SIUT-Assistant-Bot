37064d8c65cee0d49b40eb7e27540f55c37eb8c5
Islom <salomshox@gmail.com>
2022-11-14 04:05:50 +0500

    path changed again

--- start.py
+++ start.py
@@ -16,7 +16,7 @@ def main():
     kun = today.strftime("%d-%m-%Y")
     path = (os.path.abspath(os.getcwd()))
     print(path)
-    f = open(path+'staffs.json')
+    f = open(path+'./staffs.json')
     data=json.load(f)
 
     for i in data['staffs']:
